3a2c5a6ba02dccf32e5f25306e4f06f3aac57e09
cool9595 <ivko2016him@gmail.com>
2026-03-04 22:15:40 +0200

    output: colorize name using rich markup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from rich import print
 
 from config import APP_NAME, VERSION
-from utils import normalize_name, format_greeting
+from utils import normalize_name, rich_greeting
 
 def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser(prog="greet", description="Simple greeting CLI")
@@ -18,7 +18,7 @@ def main() -> None:
         return
 
     name = normalize_name(args.name)
-    print(format_greeting(name, caps=args.caps))
+    print(rich_greeting(name, caps=args.caps))
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -7,3 +7,8 @@ def format_greeting(name: str, caps: bool = False) -> str:
     """Return a greeting string."""
     msg = f"Hello, {name}!"
     return msg.upper() if caps else msg
+
+def rich_greeting(name: str, caps: bool = False) -> str:
+    """Return a greeting string with Rich markup for the name."""
+    msg = f"Hello, [cyan]{name}[/cyan]!"
+    return msg.upper() if caps else msg
